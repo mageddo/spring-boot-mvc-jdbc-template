556bc95c665a8c2a45254d9e2ff70ee7ac3daf4c
Elvis <edigitalb@gmail.com>
2016-08-13 01:12:00 -0300

    setting h2 up

--- src/main/java/com/mageddo/Application.java
+++ src/main/java/com/mageddo/Application.java
@@ -2,16 +2,19 @@ package com.mageddo;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.mageddo.entity.CustomerEntity;
+import org.h2.tools.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -33,6 +36,16 @@ public class Application {
 
     @Inject
     JdbcTemplate jdbcTemplate;
+
+    @Bean(destroyMethod = "stop", initMethod = "start")
+    public Server h2TCPServer() throws SQLException {
+        return Server.createTcpServer("-tcp","-tcpAllowOthers","-tcpPort","9491");
+    }
+
+    @Bean(destroyMethod = "stop", initMethod = "start")
+    public Server h2WebServer() throws SQLException {
+        return Server.createWebServer("-web","-webAllowOthers","-webPort","9492");
+    }
 
     @PostConstruct
     public void run() throws Exception {
