43824f404918070fedafbc6a69406922d5202427
Elvis <edigitalb@gmail.com>
2016-08-25 01:47:59 -0300

    tmp

--- src/main/java/com/mageddo/Application.java
+++ src/main/java/com/mageddo/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 @ComponentScan
 @Configuration
 public class Application {
