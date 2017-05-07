c33a23c2392ed836dfcf1f1b7b69e5b4f565cad3
Elvis de Freitas <edigitalb@gmail.com>
2017-05-07 15:35:12 -0300

    setup simple java app with spring jdbc without spring context

--- src/main/java/com/mageddo/Application.java
+++ src/main/java/com/mageddo/Application.java
@@ -1,26 +1,114 @@
 package com.mageddo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import com.mageddo.utils.DBUtils;
+import com.mageddo.utils.DefaultTransactionDefinition;
+import org.springframework.beans.factory.annotation.CustomAutowireConfigurer;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.core.env.*;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 
-/**
- * Created by lvasek on 17/04/15.
- */
-@SpringBootApplication
-@EnableAspectJAutoProxy
-@EnableAutoConfiguration
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
 @EnableTransactionManagement
-@ComponentScan
-@Configuration
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        final ServiceLoaderFactoryBean serviceLoaderFactoryBean = new ServiceLoaderFactoryBean();
+//        serviceLoaderFactoryBean.setBeanClassLoader(ClassUtils.getDefaultClassLoader());
+//        serviceLoaderFactoryBean.setSingleton(true);
+
+
+
+        final DefaultListableBeanFactory listableBeanFactory = new DefaultListableBeanFactory();
+        listableBeanFactory.setBeanClassLoader(ClassUtils.getDefaultClassLoader());
+
+        final CustomAutowireConfigurer configurer = new CustomAutowireConfigurer();
+        configurer.postProcessBeanFactory(listableBeanFactory);
+
+
+        final TransactionTemplate template = new TransactionTemplate(
+          DBUtils.getTx(),
+          new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_REQUIRED, TransactionDefinition.ISOLATION_DEFAULT)
+        );
+
+        final StandardEnvironment env = new StandardEnvironment();
+
+        final AbstractEnvironment commandLine = new AbstractEnvironment(){
+            @Override
+            protected void customizePropertySources(MutablePropertySources propertySources) {
+                super.customizePropertySources(propertySources);
+                propertySources.addLast(new SimpleCommandLinePropertySource(args));
+            }
+        };
+        env.merge(commandLine);
+
+        final AbstractEnvironment profile = new AbstractEnvironment(){
+            @Override
+            protected void customizePropertySources(MutablePropertySources propertySources) {
+                super.customizePropertySources(propertySources);
+
+                try {
+                    Properties properties;
+                    properties = loadProfileProperties("");
+                    if (properties != null){
+                        propertySources.addLast(new PropertiesPropertySource("default", properties));
+                    }
+                    final String activeProfiles = env.getProperty("spring-profiles-active");
+                    for ( final String profile : activeProfiles.split(",") ){
+
+                        properties = loadProfileProperties(profile);
+                        if (properties != null){
+                            propertySources.addLast(new PropertiesPropertySource(profile, properties));
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            private Properties loadProfileProperties(String profileName) throws IOException {
+
+                final InputStream profileIn = ClassUtils
+                    .getDefaultClassLoader()
+                    .getResourceAsStream(getPropertiesName(profileName));
+
+                if (profileIn == null){
+                    return null;
+                }
+                final Properties properties = new Properties();
+                properties.load(profileIn);
+                return properties;
+            }
+
+            private String getPropertiesName(String profileName) {
+                return "application" + (StringUtils.isEmpty(profileName) ? "" : "-" + profileName) + ".properties";
+            }
+        };
+        env.merge(profile);
+
+
+
+        template.execute(ts -> {
+           return DBUtils.getTemplate().queryForList("SELECT 1 FROM DUAL");
+        });
+
+
+//        listableBeanFactory.getBean()
+
+//        configurer.postProcessBeanFactory();
+
+//        new BeanFactoryDataSourceLookup()
+//        serviceLoaderFactoryBean.
+//        BeanFactoryUtils.beanOfTypeIncludingAncestors()
     }
+
+
 
 }
---
+++ src/main/java/com/mageddo/utils/DBUtils.java
@@ -0,0 +1,57 @@
+package com.mageddo.utils;
+
+import org.h2.Driver;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.embedded.DataSourceFactory;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+/**
+ * Created by elvis on 07/05/17.
+ */
+public class DBUtils {
+
+	private static final DataSourceTransactionManager manager;
+	private static final DataSource datasource;
+	private static final JdbcTemplate template;
+
+	static {
+
+		/*
+			Documentação das propriedades do pool https://tomcat.apache.org/tomcat-7.0-doc/jdbc-pool.html
+		 */
+		final Properties connProp = new Properties();
+		connProp.setProperty("username", "sa");
+		connProp.setProperty("password", "");
+
+		datasource = new SimpleDriverDataSource(new Driver(), "jdbc:h2:mem:test", connProp);
+
+//		final org.apache.tomcat.jdbc.pool.DataSourceFactory sourceFactory = new org.apache.tomcat.jdbc.pool.DataSourceFactory();
+//		connProp
+//		try {
+//			sourceFactory.createDataSource(connProp);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+
+		manager = new DataSourceTransactionManager(datasource);
+		template = new JdbcTemplate(datasource);
+	}
+
+	public static PlatformTransactionManager getTx(){
+		return manager;
+	}
+
+	public static JdbcTemplate getTemplate(){
+		return template;
+	}
+
+	public static DataSource getDatasource() {
+		return datasource;
+	}
+}
