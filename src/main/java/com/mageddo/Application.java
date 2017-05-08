e54b8c534fadacf608a27e1e8c0df944732c266a
Elvis de Freitas <edigitalb@gmail.com>
2017-05-07 22:03:40 -0300

    adding try

--- src/main/java/com/mageddo/Application.java
+++ src/main/java/com/mageddo/Application.java
@@ -3,6 +3,9 @@ package com.mageddo;
 import com.mageddo.controller.CustomerController;
 import com.mageddo.utils.SpringUtils;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.loader.WebappClassLoader;
+import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.core.env.Environment;
 import org.springframework.util.ClassUtils;
@@ -15,21 +18,32 @@ public class Application {
 		// http://www.jofre.de/?p=1227
 		SpringUtils.prepareEnv(args);
 
+		final Tomcat tomcat = new Tomcat();
 		final Environment env = SpringUtils.getEnv();
-		final Tomcat servletContainer = new Tomcat();
-
-
 		final String ctx = env.getProperty("server.context-path");
 		final String docBase = ClassUtils.getDefaultClassLoader().getResource("webapp").getFile();
+
+		final StandardContext context = new StandardContext();
+		context.setName(ctx);
+		context.setPath(docBase);
+		context.addLifecycleListener(new Tomcat.FixContextListener());
+		context.setParentClassLoader(ClassUtils.getDefaultClassLoader());
+		final WebappLoader loader = new WebappLoader(context.getParentClassLoader());
+		loader.setLoaderClass(WebappClassLoader.class.getName());
+		
+		context.setLoader(loader);
+		tomcat.getHost().addChild(context);
+
+
 //		final String docBase = "/home/system/Dropbox/dev/projects/spring-boot-mvc-jdbc-template/build/classes";
-		servletContainer.addContext(ctx, docBase);
-//		servletContainer.addWebapp(servletContainer.getHost(), "/tmp", docBase);
-		final CustomerController customerServlet = new CustomerController();
-		servletContainer.addServlet(ctx, customerServlet.getClass().getSimpleName(), customerServlet);
+//		tomcat.addContext(ctx, docBase);
+//		tomcat.addWebapp(tomcat.getHost(), "/tmp", docBase);
+//		final CustomerController customerServlet = new CustomerController();
+//		tomcat.addServlet(ctx, customerServlet.getClass().getSimpleName(), customerServlet);
+//
+		tomcat.setPort(Integer.parseInt(env.getProperty("server.port")));
+		tomcat.start();
 
-		servletContainer.setPort(Integer.parseInt(env.getProperty("server.port")));
-		servletContainer.start();
-
-		servletContainer.getServer().await();
+		tomcat.getServer().await();
 	}
 }
